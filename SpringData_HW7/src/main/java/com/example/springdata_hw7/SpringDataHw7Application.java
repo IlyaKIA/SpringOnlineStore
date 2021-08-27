ee6d9ebb89d1d49ed9ab54ba66b8632e4db09c6a
Ilya <ilyaKIA03@gmail.com>
2021-08-27 17:39:41 +0300

    project adapted to spring data and adding spring data  requests

---
+++ SpringData_HW7/src/main/java/com/example/springdata_hw7/SpringDataHw7Application.java
@@ -0,0 +1,13 @@
+package com.example.springdata_hw7;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataHw7Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringDataHw7Application.class, args);
+    }
+
+}
---
+++ SpringData_HW7/src/test/java/com/example/springdata_hw7/SpringDataHw7ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springdata_hw7;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataHw7ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
